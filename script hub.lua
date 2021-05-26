45a7418a98b9a75036cd3354a98c0f47c333c9ac
VoidedThoughts <53360263+VoidedThoughts@users.noreply.github.com>
2021-05-26 05:38:13 -0700

    Create script hub.lua

---
+++ script hub.lua
@@ -0,0 +1,22 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zopac/lua-stuff/main/.lua", true))()
+
+--SCRIPTS
+
+-- LOTBS: Legend Of The Bone Sword
+-- Robeats: Robeats, Robeats Community Server, basically any game thats similar to robeats
+-- Bark: Lumber Tycoon 2 
+-- SkoopX: Lumber Tycoon 2 
+-- Dungeon Quest Coin Collect: Dungeon Quest
+-- Kix Hub: Your Bizarre Adventure
+-- Rem Hub: Your Bizarre Adventure, King Piece
+-- YBA Gui: Your Bizarre Adventure
+-- Vesteria Pear Sell: Vesteria
+-- AOT Last Breath Killaura: Attack On Titan: Last Breath
+-- Nullware: all of them its an animations script
+-- NetBypass: the yeah
+
+--HUBS
+
+-- Divinity Hub: Phantom Forces, Bloxburg, Arsenal, Bad Business, Strucid, Project: Lazarus, Jailbreak, probably forgot a few but i dont give a shit it works on fucking every fps game ever
+-- Bruh Hub: Phantom Forces, Shindo Life 2, Vesteria, Ragdoll Engine, Ro-Ghoul, Project: Lazarus
+-- OwlHub: just look at this https://github.com/SiLeNSwOrD/OwlHub/blob/master/Games.txt i cant be bothered to type all of them
